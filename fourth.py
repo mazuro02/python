8c49e4fbd997021e5a866465fff178ff1e33d2e1
mazuro02 <joao.barszcz@escola.pr.gov.br>
2025-10-08 09:35:13 -0300

    Create fourth.py

---
+++ fourth.py
@@ -0,0 +1,3 @@
+import pandas as pd
+dados = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vResfZtKfATsuGT-EgsyFoPaezv8XxIBx_PRpQRd-U3-uyIG2bQzA_-zmp9QEUhz2S6361yOTzsf6h9/pub?gid=373547015&single=true&output=csv")
+dados.describe()
